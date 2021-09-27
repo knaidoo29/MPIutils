42f56dfe015314aebea8f3524e36f7c00c743837
Krishna Naidoo <krishna@krish-macbook.local>
2021-09-27 13:59:59 +0200

    basic MPI class

---
+++ mpiutils/mpiclass.py
@@ -0,0 +1,85 @@
+from mpi4py import MPI as mpi
+
+from . import loops
+
+
+class MPI:
+
+
+    def __init__(self):
+        """Initialises MPI."""
+        self.comm = mpi.COMM_WORLD
+        self.rank = self.comm.Get_rank()
+        self.size = self.comm.Get_size()
+        self.loop_size = None
+        self.mpi_info = 'Proc ' + str(self.rank+1)+' of ' + str(self.size)
+
+
+    def wait(self):
+        """Makes all jobs wait so they are synchronised."""
+        self.comm.Barrier()
+
+
+    def set_loop(self, loop_size):
+        """Sets the size of a distributed loop."""
+        self.loop_size = loop_size
+        return loops.get_MPI_loop_size(loop_size, self.size)
+
+
+    def mpi_ind2ind(self, mpi_ind):
+        """Converts the MPI_ind of a distributed loop to the index of a full loop."""
+        return loops.MPI_ind2ind(mpi_ind, self.rank, self.size, self.loop_size)
+
+
+    def clean_loop(self):
+        """Gets ride of loop_size definition."""
+        self.loop_size = None
+
+
+    def mpi_print(self, string):
+        """Prints out a string using flush so it prints out immediately in an MPI
+        setting."""
+        print(string, flush=True)
+
+
+    def send(self, data, to_rank=None, tag=11):
+        """Sends data from current core to other specified or all cores.
+
+        Parameters
+        ----------
+        data : array
+            Data to send.
+        to_rank : int, optional
+            Specify rank to send data to, or leave as None to send to all cores.
+        tag : int, optional
+            Sending tag to ensure the right data is being transfered.
+        """
+        if to_rank is not None:
+            self.comm.send(data, dest=to_rank, tag=tag)
+        else:
+            for i in range(0, self.size):
+                if i != self.rank:
+                    self.comm.send(data, dest=i, tag=tag)
+
+
+    def recv(self, source, tag=11):
+        """Receive data from another node.
+
+        Parameters
+        ----------
+        source : int
+            Source of the data.
+        tag : int
+            Sending tag to ensure the right data is being transfered.
+
+        Returns
+        -------
+        data : array
+            Data received.
+        """
+        data = self.comm.recv(source=source, tag=tag)
+        return data
+
+    def end(self):
+        """Ends MPI environment."""
+        mpi.Finalize()
---
+++ setup.py
@@ -0,0 +1,38 @@
+import setuptools
+from numpy.distutils.core import setup, Extension
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+
+with open(path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
+
+setup(name = 'mpiutils',
+      version = '0.0.0',
+      description       = "mpi4py utility functions.",
+      long_description  = long_description,
+      long_description_content_type = 'text/markdown',
+      url               = 'https://github.com/knaidoo29/mpiutils',
+      author            = "Krishna Naidoo",
+      author_email      = "krishna.naidoo.11@ucl.ac.uk",
+      license='MIT',
+      packages=setuptools.find_packages(),
+      install_requires=['numpy'],
+      #ext_modules = exts,
+      python_requires = '>=3',
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Astronomy',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Simulation',
+      ],
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      )
